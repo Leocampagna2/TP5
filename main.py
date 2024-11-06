059dc2389247d3329e5b3445b7eb77dc9f637c76
Leo Campagna <c22008803@V-PP-47-L-026.salsa.univ-amu.fr>
2024-11-06 09:34:16 +0100

    modif constructeur

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 class Point:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        p = Point(self.x, self.y)
+    def __init__(self, *args):
+        if len(args) == 2:
+            self.x = args[0]
+            self.y = args[1]
+        elif len(args) == 1:
+            self.x = args[0].x
+            self.y = args[0].y
+        elif len(args) == 0:
+            self.x = 0
+            self.y = 0
+
+
+p = Point(1,2)
+d = Point(p)
+
